008f8bb8f2f80e64e7daaef970c266f594a615ab
Stanley Filipe <stanley92filipe@gmail.com>
2022-06-18 02:57:38 -0300

    feat: update signIn method

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -1,4 +1,5 @@
 import { createContext, ReactNode, useContext } from "react";
+import { api } from "../services/api";
 
 type SignInCredentials = {
   email: string
@@ -21,7 +22,17 @@ export function AuthProvider({ children }: AuthProviderProps) {
   const isAuthenticated = false
 
   async function signIn({ email, password } : SignInCredentials): Promise<void> {
-    console.log({ email, password })
+    try {
+      const response = await api.post('sessions', {
+        email,
+        password
+      })
+
+      console.log(response.data)
+
+    } catch (err) {
+      console.log(err)
+    }
   }
 
   return (
